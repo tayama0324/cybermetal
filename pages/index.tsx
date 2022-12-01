8823c93fa063b41d74113b53b7c439de5b6480cf
nanocloudx <nanocloudx@gmail.com>
2022-12-01 20:13:54 +0900

    add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,55 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <div>
-      <p>Welcome to underground...</p>
-    </div>
+    <main>
+      <h1>サイバーメタルチンピラ</h1>
+      <h2>kinoppyd</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/kinoppyd/1'}>page1</Link></li>
+      </ul>
+      <h2>tayama0324</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/tayama/1'}>page1</Link></li>
+      </ul>
+      <h2>kusano_k</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/kusano/1'}>page1</Link></li>
+      </ul>
+      <h2>かえで@office_acer</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/sekiyasu/1'}>page1</Link></li>
+      </ul>
+      <h2>冬椿</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/fuyutsubaki/1'}>page1</Link></li>
+      </ul>
+      <h2>物入れ狂戦士</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/pokeba/1'}>page1</Link></li>
+      </ul>
+      <h2>おめが</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/omega/1'}>page1</Link></li>
+      </ul>
+      <h2>ながいたん</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nagaitan/1'}>page1</Link></li>
+      </ul>
+      <h2>daichiro.eth</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/daichiro/1'}>page1</Link></li>
+      </ul>
+      <h2>なのくろ</h2>
+      <ul style={{display: 'flex'}}>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/1'}>page1</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/2'}>page2</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/3'}>page3</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/4'}>page4</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/5'}>page5</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/6'}>page6</Link></li>
+        <li style={{padding: '0 0.5rem'}}><Link href={'/nanocloudx/7'}>page7</Link></li>
+      </ul>
+    </main>
   )
 }
---
+++ pages/mokuji.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Welcome to underground...</p>
+    </div>
+  )
+}
