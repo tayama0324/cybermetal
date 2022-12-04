722cffbf761f5f834c7d91040f7881fe38e4ab1a
nanocloudx <nanocloudx@gmail.com>
2022-12-05 02:10:30 +0900

    add tayama article

--- components/Image.tsx
+++ components/Image.tsx
@@ -4,11 +4,12 @@ import s from './Image.module.scss'
 type Props = {
   href: string
   description?: string
+  widthPercentage?: number
 }
 
 export const Image: React.FC<Props> = (props) => {
   return (
-    <div className={s.main}>
+    <div className={s.main} style={{width: `${props.widthPercentage}%` || '100%'}}>
       <img className={s.image} src={props.href} />
       {props.description && (<p className={s.description}>{props.description}</p>)}
     </div>
---
+++ pages/tayama/7.tsx
@@ -0,0 +1,34 @@
+import {Title} from '../../components/Title'
+import {Image} from '../../components/Image'
+
+export default function Page() {
+  return (
+    <div>
+      <p>
+        尾崎放哉の句『咳をしても一人』も同様のリズムを持ち、同様の効果をもたらしていると言えるでしょう。自由律俳句は多様であり、必ずしもこのようなリズムに当てはめられるものばかりではありませんが、リズムを捉えることは自由律俳句を深く楽しむ上で一定の助けになるものと思います。
+      </p>
+      <h2>結び</h2>
+      <p>
+        五・七・五はモーラ等時性という性質により表拍・裏拍・表拍で終わることになり、これが進行と終止のリズムをバランス良く織り交ぜているため心地よく感じるのだという筆者の主張を説明しました。またこの論によると、字余りや句またがりのような定形を外れた形式についても、標準的な五・七・五と同様に心地よく感じることを矛盾なく説明できることを主張しました。
+      </p>
+      <p>
+        近年575検出（日常会話やテキストコミュニケーションにおいて、発言のうち五・七・五になっている部分を検出して指摘する知的なゲーム）が流行しています [要出典]。575検出を苦手とする人も少なからずいるようですが、会話の中で心地よく感じるリズムに着目することは575検出の助けになるものと思いますし、また筆者の周囲の575検出が得意な人には音楽センスに長けている人が多いように感じます。
+      </p>
+      <p>
+        この記事が読者の楽しい575生活・音楽生活の一助となることを願ってやみません。
+      </p>
+      <h2>参考文献</h2>
+      <ul>
+        <li>[^1] いずれも松尾芭蕉 (1644-1694) の句。</li>
+        <li>[^2] バニラ求人テーマソング（作詞・作曲者非公開、【公式】バニラチャンネル）[2017] https://www.youtube.com/watch?v=ow6Q0zRa4Xw</li>
+        <li>[^3] Trancing Pulse （作詞：AJURIKA　作曲：上松範康　編曲：藤永龍太郎）[2015] https://www.youtube.com/watch?v=QwuEEA1jSCQ</li>
+        <li>[^4] 夜に駆ける（作詞作曲：Ayase）[2019] https://www.youtube.com/watch?v=by4SYYWlhEs</li>
+        <li>[^5] 威風堂々（原題：Pomp and Circumstance 作曲：Edward W. Elgar）[1901]</li>
+        <li>[^6] ワンダーステラ（作詞：林 英樹　作曲：佐藤純一　編曲：fhána）[2015] https://www.youtube.com/watch?v=rHpzTO_P9xw</li>
+        <li>[^7] モーラ, 音節, リズムの心理言語学的考察 (河野守夫、Journal of the Phonetic Society of Japan) [1998] https://www.jstage.jst.go.jp/article/onseikenkyu/2/1/2_KJ00007630824/_pdf</li>
+        <li>[^8] https://twitter.com/smashedavolove/status/1109463546381983746</li>
+        <li>[^9] 【登録者200万人！】歌ったり語ったりする【ホロライブ/宝鐘マリン】 44:38~ https://www.youtube.com/watch?v=waBnG141kqE</li>
+      </ul>
+    </div>
+  )
+}
